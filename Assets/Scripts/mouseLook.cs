48619f89535d6fb8a6a0c17e9718371bc853cdb7
specificallykate <katesuzannah@gmail.com>
2021-01-29 19:56:24 -0500

    Pushing unity project

---
+++ Assets/Scripts/mouseLook.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour {
+
+	float upDownLook = 0f;
+	float mouseSensitivity = 1000f;
+	float rotationX;
+
+	void Start () {
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+		
+	void Update () {
+
+		// 0 . Lock mouse
+		if (Input.GetMouseButton(0)) {
+			Cursor.lockState = CursorLockMode.Locked; //lock cursor in middle of screen
+			Cursor.visible = false;
+		}
+
+		// 1. Get mouse input data
+		float mouseX = Input.GetAxis ("Mouse X") * Time.deltaTime * mouseSensitivity; //horizontal mouseSpeed
+		float mouseY = Input.GetAxis ("Mouse Y") * Time.deltaTime * mouseSensitivity; //vertical mouseSpeed
+
+		rotationX += mouseX;
+
+		// 2. Rotate the camera
+		transform.parent.localEulerAngles = new Vector3 (transform.parent.localEulerAngles.x, rotationX, 0f);
+
+		// 2b. Clamp/constrain the X angle (pitch) so we can't look too far up or down
+		upDownLook -= mouseY;
+		upDownLook = Mathf.Clamp (upDownLook, -80f, 80f); //Constrain look 80 degrees up or down
+
+		// 3. Unroll the camera
+		transform.localEulerAngles = new Vector3(upDownLook, transform.localEulerAngles.y, 0f);
+	}
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+	CharacterController playerCharCon;
+
+	Vector3 moveVector;
+	public float moveSpeed = 5f;
+
+
+	void Start () {
+		playerCharCon = GetComponent<CharacterController>();
+	}
+	
+
+	void Update () {
+		float frontBack = Input.GetAxis("Vertical") * moveSpeed;
+		float leftRight = Input.GetAxis("Horizontal") * moveSpeed;
+		playerCharCon.Move(transform.forward * frontBack * Time.deltaTime);
+		playerCharCon.Move(transform.right * leftRight * Time.deltaTime);
+		// Add gravity
+		playerCharCon.Move(Physics.gravity*.008f); //move the controller down
+	}
+}
